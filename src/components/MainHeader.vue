603d27f7e382a9710ea4232231316ec82d46aa46
devwithu <devwithu@naver.com>
2021-07-19 14:08:35 +0900

    MainHeader

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="bg-customBlue">
-    This is my main component
+  <div class="MainHeader container mx-auto md:bg-contain bg-no-repeat bg-cover bg-left-top md:bg-center relative -top-32 md:-top-16" :style="`background-image: url(${PorscheBackground}); height: 100vh;`">
+
+
   </div>
 </template>
+
+<script setup>
+import PorscheBackground from '../assets/porsche-model.png'
+</script>
